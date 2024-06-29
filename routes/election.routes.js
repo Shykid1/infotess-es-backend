a91f4877ecc9112f198e5ba849b8e961323f8165
Yussif Yahuza <yussifyahuza12@gmail.com>
2024-06-29 21:50:12 +0100

    Added election routes

---
+++ routes/election.routes.js
@@ -0,0 +1,26 @@
+const electionRouter = require("express").Router();
+const { protect, admin } = require("../middlewares/auth.middleware");
+const {
+  createElection,
+  getElections,
+  getElectionById,
+  updateElection,
+  deleteElection,
+} = require("../controllers/election.controller");
+
+// Create a new Election
+electionRouter.post("/", protect, admin, createElection);
+
+// Get all Elections
+electionRouter.get("/", protect, admin, getElections);
+
+// Get an Election by ID
+electionRouter.get("/:id", protect, admin, getElectionById);
+
+// Update an Election
+electionRouter.put("/:id", protect, admin, updateElection);
+
+// Delete an Election
+electionRouter.delete("/:id", protect, admin, deleteElection);
+
+module.exports = electionRouter;
