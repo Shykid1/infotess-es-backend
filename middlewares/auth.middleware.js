b97f50a54ca96c68a3db2494fd8f511f219fad1c
Yussif Yahuza <yussifyahuza12@gmail.com>
2024-06-29 02:08:33 +0100

    feat: Add role field to user model with default value
    The code changes add a new "role" field to the user model in the `user.model.js` file. The field is of type string and has an enum validation for the values "super", "admin", and "voter". The default value for the field is set to "voter".

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,3 @@
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+const User = require("../models/user.model");
--- models/user.model.js
+++ models/user.model.js
@@ -11,6 +11,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    role: {
+      type: String,
+      enum: ["super", "admin", "voter"],
+      default: "voter",
+    },
   },
   { timestamps: true }
 );
