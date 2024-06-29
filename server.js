c62a41186564d5d090c497cdacce353fbb8ade59
Yussif Yahuza <yussifyahuza12@gmail.com>
2024-06-29 22:39:03 +0100

    feat: Add routes for user, votes, election, and candidate
    This commit adds routes for user, votes, election, and candidate in the server.js file. These routes are mounted under the respective API versions and will handle the corresponding HTTP requests. This update enhances the functionality of the application by providing endpoints for user management, voting, election management, and candidate management.
    
    Recent user commits:
    - Add electionId field to candidate model
    - Finished Auth Controller
    
    Recent repository commits:
    - feat: Added authentication middleware
    - feat: Add role field to user model with default value
    - Project Initialization && Creation of Models

--- server.js
+++ server.js
@@ -7,18 +7,22 @@ const dbConnect = require("./utils/dbConnect");
 dotenv.config();
 
 // Connect to MongoDB
-
 dbConnect();
 
 const port = process.env.PORT || 3000;
 
 //initializa app
-
 const app = express();
 
 //middleware
-
 app.use(cors());
 app.use(express.json());
+
+//routes
+app.use("/api/v1/auth", require("./routes/auth.routes"));
+app.use("/api/v1/user", require("./routes/user.routes"));
+app.use("/api/v1/candidate", require("./routes/candidate.routes"));
+app.use("/api/v1/election", require("./routes/election.routes"));
+app.use("/api/v1/vote", require("./routes/vote.routes"));
 
 app.listen(port, () => console.log(`Server started on PORT: ${port}`));
