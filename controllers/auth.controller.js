d29b61cc663e008b891077578657b87a265125e4
Yussif Yahuza <yussifyahuza12@gmail.com>
2024-07-01 23:38:51 +0000

    feat: Refactor voter login function
    The code changes in the `auth.controller.js` file remove an unnecessary line of code in the `voterLogin` function. This improves the efficiency of the login process for voters.

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -179,7 +179,6 @@ exports.voterLogin = async (req, res) => {
     if (!isMatch) {
       return res.status(400).json({ message: "Invalid credentials" });
     }
-    
 
     // Generate token
     const token = jwt.sign({ id: voter.user._id }, process.env.JWT_SECRET, {
